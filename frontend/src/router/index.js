e7c052b39b697af459082f4fa3914d43e89c248a
guoxingx <3064793912@qq.com>
2018-05-10 18:56:27 +0800

    前端

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -15,24 +15,21 @@ export default new Router({
       name: 'blogs',
       component: function (resolve) {
         require(['@/views/blog_list'], resolve)
-      },
-      meta: { title: 'gxblog' }
+      }
     },
     {
       path: '/blogs/:id',
       name: 'blog',
       component: function (resolve) {
         require(['@/views/blog'], resolve)
-      },
-      meta: { title: 'gxblog' }
+      }
     },
     {
       path: '/betonether/:id',
       name: 'betonether',
       component: function (resolve) {
         require(['@/views/betonether'], resolve)
-      },
-      meta: { title: 'gxblog-betonether' }
+      }
     }
   ]
 })
